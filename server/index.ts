5b3e072d526c3d2b047ae00e6ff92e4eb85bba9a
keenwang <keenwang@zego.im>
2022-02-23 01:13:42 +0800

    🎉   init project

---
+++ server/index.ts
@@ -0,0 +1,75 @@
+import express from 'express'
+import http from 'http'
+import { Server as SocketIO, Socket } from 'socket.io'
+import cors from 'cors'
+
+const app = express()
+app.use(cors())
+// 解决跨域
+app.all('*', function (req, res, next) {
+  // app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With')
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
+  if (req.method === 'OPTIONS') {
+    res.send(200)
+  } else {
+    next()
+  }
+})
+
+const maxUserCount = 2
+
+const server = http.createServer(app)
+server.listen(9966, () => {
+  // @ts-ignore
+  const host = server.address().address
+  // @ts-ignore
+  const port = server.address().port
+  console.log(`应用实例，访问地址为 http://${host}:${port}`)
+  console.log('server is running', 'ws://localhost:8866')
+})
+const io = new SocketIO(server, {
+  cors: {
+    origin: '*'
+  }
+})
+io.on('connection', (client) => {
+  console.log('client connect')
+  // 收到 message 进行转发
+  client.on('message', (roomID, message) => {
+    console.log('message', roomID, message)
+    client.to(roomID).emit('message', roomID, message)
+  })
+
+  client.on('join', (roomID) => {
+    // @ts-ignore
+    const myRoom: any = io.sockets.adapter.rooms.get(roomID)
+    // 获取房间人数
+    let users = myRoom ? myRoom.size : 0
+    if (users < maxUserCount) {
+      client.join(roomID)
+      client.emit('joined', roomID, client.id)
+      users = myRoom ? myRoom.size : 0
+      if (users > 1) {
+        client.to(roomID).emit('other_join', roomID, client.id)
+      }
+    } else {
+      client.emit('full', roomID, client.id)
+    }
+  })
+
+  client.on('leave', (roomID) => {
+    client.leave(roomID)
+    const myRoom: any = io.sockets.adapter.rooms.get(roomID)
+    // 获取房间人数
+    const users = myRoom ? Object.keys(myRoom.sockets).length : 0
+    console.log('current user count: ', users)
+    client.to(roomID).emit('bye', roomID, client.id)
+    client.emit('left', roomID, client.id)
+  })
+  client.on('event', (data: any) => {
+    console.log('data', data)
+  })
+  client.on('disconnect', () => { /* … */ })
+})
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,279 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <input v-model="state.roomId" type="text" placeholder="roomId" />
+  <button @click="enterRoom" :disabled="state.userState !== 'init'">
+    进入房间
+  </button>
+  <button @click="leaveRoom" :disabled="state.userState === 'init'">
+    退出房间
+  </button>
+  本地视频
+  <video muted :srcObject="state.localStream" controls autoplay playsinline></video>
+  远端视频
+  <video muted :srcObject="state.remoteStream" controls autoplay playsinline></video>
 </template>
 
-<style lang="less">
+<script lang="ts">
+// import HelloWorld from './components/HelloWorld.vue';
+import { onMounted, reactive } from 'vue'
+import { io as socketIO, Socket } from 'socket.io-client'
+
+enum UserState {
+  Init = 'init',
+  Joined = 'joined',
+  Connected = 'joined_conn',
+  Unbind = 'joined_unbind',
+}
+export default {
+  setup () {
+    // let deviceList: MediaDeviceInfo[] = reactive([]);
+    // let localStream: MediaStream|null = null;
+    const state: {
+      deviceList: MediaDeviceInfo[];
+      localStream?: MediaStream;
+      localStreamList: MediaStream[];
+      remoteStream?: MediaStream;
+      roomId: string;
+      userId: string;
+      userState: UserState;
+      io?: Socket;
+      peerConnection?: RTCPeerConnection;
+      pcInfo: {
+        offer: any;
+        answer: any;
+      };
+    } = reactive({
+      deviceList: [],
+      roomId: 'video',
+      userId: '',
+      userState: UserState.Init,
+      localStreamList: [],
+      pcInfo: {
+        offer: '',
+        answer: ''
+      }
+    })
+
+    onMounted(async () => {
+      state.deviceList = await navigator?.mediaDevices?.enumerateDevices()
+      console.log('枚举设备', state.deviceList)
+    })
+    async function captureStream () {
+      try {
+        if (!state.localStream) {
+          state.localStream = await navigator.mediaDevices.getUserMedia({
+            video: {
+              width: 640,
+              height: 480,
+              frameRate: 15
+            },
+            audio: true
+          })
+          state.localStreamList.push(state.localStream)
+        }
+      } catch (error) {
+        console.error('getUserMedia', error)
+      }
+    }
+    function enterRoom () {
+      if (!state.io) {
+        state.io = socketIO('http://localhost:9966')
+      }
+      state.io.on('connect', () => {
+        console.log('connect Server')
+      })
+      state.io.on('joined', (roomId, id) => {
+        state.userState = UserState.Joined
+        state.userId = id
+        createPeerConnection()
+        bindTracks()
+      })
+      state.io.on('other_join', (roomId, id) => {
+        console.log(`user ${id} join in ${roomId}`)
+        if (state.userState === UserState.Unbind) {
+          createPeerConnection()
+          bindTracks()
+        }
+        state.userState = UserState.Connected
+        call()
+      })
+      function leaveRoomHandle (roomId: string) {
+        console.log(`join ${roomId} failed`)
+        hangup()
+        state.io?.close()
+        state.io = undefined
+        state.userState = UserState.Init
+      }
+      state.io.on('full', (roomId) => {
+        leaveRoomHandle(roomId)
+        alert('room is full')
+      })
+      state.io.on('left', (roomId) => {
+        leaveRoomHandle(roomId)
+        closeLocalMedia()
+      })
+      state.io.on('bye', (roomId, id) => {
+        console.log(`user ${id} leaves room ${roomId}`)
+        state.userState = UserState.Unbind
+        hangup()
+      })
+      // 处理接收的 SDP 和 candidate
+      state.io.on('message', async (roomId, data) => {
+        if (data && data.type) {
+          if (data.type === 'offer') {
+            state.pcInfo.offer = data.sdp
+            state.peerConnection?.setRemoteDescription(
+              new RTCSessionDescription(data)
+            )
+            console.log('remote offer', data)
+            const answer = await state.peerConnection?.createAnswer()
+            answer && sendAnswer(answer)
+          } else if (data.type === 'candidate') {
+            const candidate = new RTCIceCandidate({
+              sdpMLineIndex: data.label,
+              candidate: data.candidate
+            })
+            // 将远端的候选者信息添加到 pc 中
+            await state.peerConnection?.addIceCandidate(candidate)
+            console.log('add candidate', data)
+          } else if (data.type === 'answer') {
+            state.pcInfo.answer = data.sdp
+            await state.peerConnection?.setRemoteDescription(
+              new RTCSessionDescription(data)
+            )
+            console.log('remote answer', data)
+          }
+        } else {
+          console.error('message is invalid!', data)
+        }
+      })
+      // 发送登录房间消息
+      state.io.emit('join', state.roomId)
+    }
+    function createPeerConnection () {
+      console.log('createPeerConnection')
+      const config = {
+        // iceServers: [
+        //   {
+        //     urls: "stun:stun.example.org",
+        //     username: "xxx",
+        //     credential: "xxx",
+        //   },
+        // ],
+        // iceTransportPolicy: "relay",
+        // iceCandidatePoolSize: "0",
+      }
+      if (!state.peerConnection) {
+        state.peerConnection = new RTCPeerConnection(config)
+        // 接收远端媒体
+        state.peerConnection.ontrack = (e) => {
+          debugger
+          state.remoteStream = e.streams[0]
+        }
+        // 收集候选者
+        state.peerConnection.onicecandidate = (e) => {
+          if (e.candidate) {
+            sendMessage({
+              type: 'candidate',
+              label: e.candidate.sdpMLineIndex,
+              id: e.candidate.sdpMid,
+              candidate: e.candidate.candidate
+            })
+          } else {
+            console.log('no candidate')
+          }
+        }
+      } else {
+        console.log('PeerConnection has been created')
+      }
+    }
+    async function bindTracks () {
+      await captureStream()
+      // const videoTrack = state.localStream?.getVideoTracks()[0]
+      // const audioTrack = state.localStream?.getAudioTracks()[0]
+      // 发送本地媒体
+      if (state.localStream) {
+        state.localStream.getTracks().forEach((track) => {
+          console.log('bindTracks', track)
+          state.peerConnection?.addTrack(track, state.localStream!)
+        })
+        console.log('bindTracks')
+      }
+    }
+    function sendMessage (data: any) {
+      console.log('sendMessage')
+      state.io?.emit('message', state.roomId, data)
+    }
+    function hangup () {
+      // 挂起
+      console.log('hangup')
+      if (!state.peerConnection) {
+        return
+      }
+      state.pcInfo.offer = null
+      state.peerConnection.close()
+      state.peerConnection = undefined
+    }
+    async function call () {
+      // 媒体协商
+      console.log('call')
+      if (state.userState === UserState.Connected) {
+        const offerOptions = {
+          offerToReceiveAudio: 1,
+          offerToReceiveVideo: 1
+        }
+        // @ts-ignore
+        const offer = await state.peerConnection?.createOffer(offerOptions)
+        offer && sendOffer(offer)
+      }
+    }
+    function closeLocalMedia () {
+      console.log('closeLocalMedia')
+      if (state.localStream) {
+        state.localStream.getTracks().forEach((track) => {
+          track.stop()
+        })
+      }
+      state.localStream = undefined
+    }
+    function leaveRoom () {
+      state.io?.emit('leave', state.roomId)
+      hangup()
+      closeLocalMedia()
+      state.pcInfo = {
+        offer: '',
+        answer: ''
+      }
+      state.userState = UserState.Init
+    }
+    // @ts-ignore
+    function sendAnswer (desc: any) {
+      state.peerConnection?.setLocalDescription(desc)
+      state.pcInfo.answer = desc.sdp
+      sendMessage(desc)
+    }
+    // @ts-ignore
+    function sendOffer (desc: any) {
+      state.peerConnection?.setLocalDescription(desc)
+      state.pcInfo.offer = desc.sdp
+      sendMessage(desc)
+    }
+    return {
+      state,
+      enterRoom,
+      leaveRoom
+    }
+  }
+}
+</script>
+
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-top: 60px;
 }
 </style>
